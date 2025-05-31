782ae999e3c536027e6a829922fa1ad13ed804b3
DConda <dennis.muladi@gmail.com>
2025-06-01 06:31:12 +0700

    misc, unused feature to be used for future reference

--- resources/views/employer/candidates/show.blade.php
+++ resources/views/employer/candidates/show.blade.php
@@ -36,6 +36,16 @@
                         <span class="hidden unhide-text">Unhide Candidate</span>
                     </button>
                     
+                    <!-- TODO: Review UX - candidate favorite system -->
+                    <button onclick="toggleFavoriteCandidate({{ $candidate->id }}, '{{ $candidate->name }}')" 
+                            class="favorite-button opacity-0 absolute pointer-events-none bg-gray-700 hover:bg-gray-600 text-gray-400 px-4 py-2 rounded-lg inline-flex items-center transition-colors"
+                            data-candidate-id="{{ $candidate->id }}"
+                            title="Add to favorites">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+                        </svg>
+                    </button>
+                    
                     <!-- Available Badge -->
                     @if($candidate->jobseekerProfile && $candidate->jobseekerProfile->available_for_hire)
                         <div>
